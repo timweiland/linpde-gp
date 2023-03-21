253e7d4bdc5e038f86872c1f2fb5824691bb44a3
Tim Weiland <tim@weiland-lahnstein.de>
2023-03-21 13:00:46 +0100

    Register DiagPlusLowRank and RankFactorized

--- src/linpde_gp/linops/__init__.py
+++ src/linpde_gp/linops/__init__.py
@@ -2,3 +2,5 @@ from probnum.linops import *
 
 from ._block import BlockMatrix, BlockMatrix2x2
 from ._concatenated import ConcatenatedLinearOperator
+from ._rank_factorized import RankFactorizedMatrix
+from ._diag_plus_low_rank import DiagonalPlusLowRankMatrix
