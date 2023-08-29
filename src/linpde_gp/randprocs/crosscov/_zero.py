cbab2b9bb496495bdda0af1f26cd7b020fd67e30
Tim Weiland <tim@weiland-lahnstein.de>
2023-08-29 15:29:01 +0200

    Zero PVCrosscov linop implementation

--- src/linpde_gp/randprocs/crosscov/_zero.py
+++ src/linpde_gp/randprocs/crosscov/_zero.py
@@ -1,5 +1,6 @@
-from jax import numpy as jnp
 import numpy as np
+import probnum as pn
+from jax import numpy as jnp
 
 from ._pv_crosscov import ProcessVectorCrossCovariance
 
@@ -27,4 +28,18 @@ class Zero(ProcessVectorCrossCovariance):
                 if self.reverse
                 else batch_shape + self.randproc_output_shape + self.randvar_shape
             ),
+        )
+
+    def _evaluate_linop(self, x: np.ndarray) -> pn.linops.LinearOperator:
+        batch_shape = x.shape[: x.ndim - self.randproc_input_ndim]
+        output_shape = batch_shape + self.randproc_output_shape
+        output_size = np.prod(output_shape)
+
+        return pn.linops.Zero(
+            shape=(
+                (self.randvar_size, output_size)
+                if self.reverse
+                else (output_size, self.randvar_size)
+            ),
+            dtype=x.dtype,
         )
