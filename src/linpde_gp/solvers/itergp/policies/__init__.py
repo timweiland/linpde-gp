97d6be1b0cc3bb994780b7993345b441281e7a38
Tim Weiland <tim@weiland-lahnstein.de>
2023-09-14 12:52:08 +0200

    Delete VarianceReduction (move in commit tree)

--- src/linpde_gp/solvers/itergp/policies/__init__.py
+++ src/linpde_gp/solvers/itergp/policies/__init__.py
@@ -1,7 +1,3 @@
 from . import block
 from ._cg import CGPolicy, ConcretePolicy
 from ._policy import ConcretePolicy, Policy
-from ._variance_reduction import (
-    ConcreteVarianceReductionPolicy,
-    VarianceReductionPolicy,
-)
