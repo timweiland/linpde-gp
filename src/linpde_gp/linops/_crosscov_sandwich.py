6cc07c91f1e5b5077a873584e728f32d1c32d468
Tim Weiland <tim@weiland-lahnstein.de>
2023-11-06 16:15:52 +0100

    Torch implementation of CrosscovSandwich diagonal

--- src/linpde_gp/linops/_crosscov_sandwich.py
+++ src/linpde_gp/linops/_crosscov_sandwich.py
@@ -2,6 +2,7 @@ import numpy as np
 from probnum import linops
 from scipy.linalg import solve_triangular
 import torch
+import functools
 
 
 class CrosscovSandwichLinearOperator(linops.LinearOperator):
@@ -20,24 +21,29 @@ class CrosscovSandwichLinearOperator(linops.LinearOperator):
 
     def _transpose(self) -> linops.LinearOperator:
         return self
+    
+    @functools.cached_property
+    def torch_raw_factor_transposed(self) -> torch.Tensor:
+        return torch.tensor(self._sandwiched_linop.raw_factor.T).to("cuda" if torch.cuda.is_available() else "cpu")
 
-    def _get_column(self, idx: int) -> np.ndarray:
+    def _get_column(self, idx: int, device) -> np.ndarray:
         from linpde_gp.linops import (
             DenseCholeskySolverLinearOperator,
             RankFactorizedMatrix,
         )
 
         if isinstance(self._sandwiched_linop, DenseCholeskySolverLinearOperator):
-            e = np.zeros(self._sandwiched_linop.shape[1])
+            e = torch.zeros(self._sandwiched_linop.shape[1], dtype=torch.float64).to(device)
             e[idx] = 1.0
-            return solve_triangular(self._sandwiched_linop.raw_factor.T, e, lower=False)
+            return torch.linalg.solve_triangular(self.torch_raw_factor_transposed, e.reshape((e.size(dim=0), 1)), upper=True).reshape(-1)
         elif isinstance(self._sandwiched_linop, RankFactorizedMatrix):
             return self._sandwiched_linop.U[:, idx]
         return NotImplemented
 
     def _diagonal(self) -> np.ndarray:
-        diag = np.zeros(self._crosscov.shape[0])
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        diag = torch.zeros(self._crosscov.shape[0], dtype=torch.float64).to(device)
         for i in range(self._sandwiched_linop.shape[1]):
-            v = self._crosscov @ self._get_column(i)
+            v = self._crosscov @ self._get_column(i, device)
             diag += v**2
-        return diag
+        return diag.cpu().numpy()
