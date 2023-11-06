9fc672bb21b5caa1ba748c059c39f1c0530ad3e4
Tim Weiland <tim@weiland-lahnstein.de>
2023-11-06 16:20:57 +0100

    RankFactorizedMatrix: Smarter memory alloc

--- src/linpde_gp/linops/_rank_factorized.py
+++ src/linpde_gp/linops/_rank_factorized.py
@@ -3,11 +3,35 @@ from probnum.typing import DTypeLike, ShapeLike
 import numpy as np
 import torch
 
+class DynamicMatrix:
+    def __init__(self, row_size, initial_capacity=1430, device='cuda'):
+        self.data = torch.empty((row_size, initial_capacity), device=device, dtype=torch.float64)
+        self.current_size = 0
+        self.capacity = initial_capacity
+
+    def append(self, col):
+        # Check if there's space left
+        if self.current_size == self.capacity:
+            self._resize(2 * self.capacity)
+        
+        self.data[:, self.current_size] = col
+        self.current_size += 1
+
+    def _resize(self, new_capacity):
+        new_data = torch.empty((self.data.size(0), new_capacity), device=self.data.device, dtype=self.data.dtype)
+        new_data[:, :self.current_size] = self.data
+        self.data = new_data
+        self.capacity = new_capacity
+
+    def get_data(self):
+        return self.data[:, :self.current_size]
 
 class RankFactorizedMatrix(pn.linops.LinearOperator):
     def __init__(
-        self, U: np.ndarray | None, shape: ShapeLike = None, dtype: DTypeLike = None
+        self, U: np.ndarray | None, shape: ShapeLike = None, dtype: DTypeLike = None, initial_capacity=1430,
     ):
+        self._num_factors = 0
+        self._initial_capacity = initial_capacity
         if U is None:
             self._U = None
             self._U_torch = None
@@ -21,11 +45,16 @@ class RankFactorizedMatrix(pn.linops.LinearOperator):
             self._U_torch = torch.from_numpy(U).to(
                 "cuda" if torch.cuda.is_available() else "cpu"
             )
+            self._num_factors = U.shape[1]
             super().__init__(U.shape, U.dtype)
 
     @property
     def U(self) -> np.ndarray | None:
         return self._U
+    
+    @property
+    def num_factors(self) -> int:
+        return self._num_factors
 
     def _matmul(self, x: np.ndarray) -> np.ndarray:
         if self._U is None:
@@ -35,23 +64,26 @@ class RankFactorizedMatrix(pn.linops.LinearOperator):
     def _matmul_torch(self, x: torch.Tensor) -> torch.Tensor:
         if self._U_torch is None:
             return torch.zeros_like(x)
-        return self._U_torch @ (self._U_torch.T @ x)
+        mat = self._U_torch.get_data()
+        return mat @ (mat.T @ x)
 
     def append_factor(self, factor: np.ndarray):
-        if self._U is None:
+        if (self._U is None and self._U_torch is None):
             if isinstance(factor, np.ndarray):
                 self._U = factor.reshape(-1, 1)
             else:
                 # torch.Tensor
-                self._U_torch = factor.reshape(-1, 1)
+                self._U_torch = DynamicMatrix(factor.shape[0], device=factor.device, initial_capacity=self._initial_capacity)
         else:
             if isinstance(factor, np.ndarray):
                 self._U = np.concatenate((self._U, factor.reshape(-1, 1)), axis=-1)
             else:
                 # torch.Tensor
-                self._U_torch = torch.cat(
-                    (self._U_torch, factor.reshape(-1, 1)), dim=-1
-                )
+                self._U_torch.append(factor)
+        self._num_factors += 1
+    
+    def _transpose(self) -> pn.linops.LinearOperator:
+        return self
 
 
 class LowRankProduct(pn.linops.LinearOperator):
@@ -126,3 +158,6 @@ class LowRankProduct(pn.linops.LinearOperator):
                 self._V_torch = torch.cat(
                     (self._V_torch, V_factor.reshape(-1, 1)), dim=-1
                 )
+    
+    def _transpose(self) -> pn.linops.LinearOperator:
+        return LowRankProduct(self._V, self._U)
