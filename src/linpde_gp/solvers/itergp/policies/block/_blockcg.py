37851148a31e5f48b396e75477f9ec105f5d2777
Tim Weiland <tim@weiland-lahnstein.de>
2023-11-07 14:09:42 +0100

    BlockCG: Copy residual before modifying

--- src/linpde_gp/solvers/itergp/policies/block/_blockcg.py
+++ src/linpde_gp/solvers/itergp/policies/block/_blockcg.py
@@ -2,6 +2,7 @@ from typing import List, Optional, Tuple
 
 import numpy as np
 from linpde_gp.linops import BlockMatrix2x2
+import torch
 
 from ...._gp_solver import GPInferenceParams
 from ..._solver_state import SolverState
@@ -16,7 +17,10 @@ class ConcreteBlockCG(ConcreteBlockPolicy):
     def __call__(
         self, solver_state: SolverState, rng: Optional[np.random.Generator] = None
     ) -> np.ndarray:
-        residual = np.copy(solver_state.predictive_residual)
+        if isinstance(solver_state.predictive_residual, np.ndarray):
+            residual = np.copy(solver_state.predictive_residual)
+        else:
+            residual = torch.clone(solver_state.predictive_residual)
         residual[: self._block_start] = 0.0
         residual[self._block_end :] = 0.0
         return residual
