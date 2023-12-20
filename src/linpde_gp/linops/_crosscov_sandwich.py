dc8c4ab246a4244d74eccf21a045ebd15909255f
Tim Weiland <tim@weiland-lahnstein.de>
2023-12-20 15:10:26 +0100

    Neater `CrosscovSandwich` implementation

--- src/linpde_gp/linops/_crosscov_sandwich.py
+++ src/linpde_gp/linops/_crosscov_sandwich.py
@@ -4,10 +4,12 @@ from scipy.linalg import solve_triangular
 import torch
 import functools
 
+from ._outer_product import OuterProductMatrix
+
 
 class CrosscovSandwichLinearOperator(linops.LinearOperator):
     def __init__(
-        self, crosscov: linops.LinearOperator, sandwiched_linop: linops.LinearOperator
+        self, crosscov: linops.LinearOperator, sandwiched_linop: OuterProductMatrix
     ):
         self._crosscov = crosscov
         self._sandwiched_linop = sandwiched_linop
@@ -15,35 +17,52 @@ class CrosscovSandwichLinearOperator(linops.LinearOperator):
 
     def _matmul(self, x: np.ndarray) -> np.ndarray:
         return (self._crosscov @ self._sandwiched_linop @ self._crosscov.T) @ x
-    
+
     def _matmul_torch(self, x: torch.Tensor) -> torch.Tensor:
         return (self._crosscov @ self._sandwiched_linop @ self._crosscov.T) @ x
 
     def _transpose(self) -> linops.LinearOperator:
         return self
-    
-    @functools.cached_property
-    def torch_raw_factor_transposed(self) -> torch.Tensor:
-        return torch.tensor(self._sandwiched_linop.raw_factor.T).to("cuda" if torch.cuda.is_available() else "cpu")
 
-    def _get_column(self, idx: int, device) -> np.ndarray:
-        from linpde_gp.linops import (
-            DenseCholeskySolverLinearOperator,
-            RankFactorizedMatrix,
-        )
-
-        if isinstance(self._sandwiched_linop, DenseCholeskySolverLinearOperator):
-            e = torch.zeros(self._sandwiched_linop.shape[1], dtype=torch.float64).to(device)
-            e[idx] = 1.0
-            return torch.linalg.solve_triangular(self.torch_raw_factor_transposed, e.reshape((e.size(dim=0), 1)), upper=True).reshape(-1)
-        elif isinstance(self._sandwiched_linop, RankFactorizedMatrix):
-            return self._sandwiched_linop.U[:, idx]
-        return NotImplemented
-
-    def _diagonal(self) -> np.ndarray:
+    def _diagonal(self, compute_iterative=True) -> np.ndarray:
         device = "cuda" if torch.cuda.is_available() else "cpu"
         diag = torch.zeros(self._crosscov.shape[0], dtype=torch.float64).to(device)
-        for i in range(self._sandwiched_linop.shape[1]):
-            v = self._crosscov @ self._get_column(i, device)
-            diag += v**2
+        if compute_iterative:
+            for i in range(self._sandwiched_linop.U.shape[1]):
+                e = torch.zeros(
+                    self._sandwiched_linop.U.shape[1], dtype=torch.float64
+                ).to(device)
+                e[i] = 1.0
+                if self._sandwiched_linop.is_symmetric:
+                    diag += (self._crosscov @ self._sandwiched_linop.U @ e) ** 2
+                else:
+                    diag += (self._crosscov @ self._sandwiched_linop.U @ e) * (
+                        self._crosscov @ self._sandwiched_linop.V @ e
+                    )
+        else:
+            if self._sandwiched_linop.is_symmetric:
+                cols = (
+                    self._crosscov
+                    @ self._sandwiched_linop.U
+                    @ torch.eye(
+                        self._sandwiched_linop.U.shape[1], dtype=torch.float64
+                    ).to(device)
+                )
+                diag = torch.sum(cols**2, dim=-1)
+            else:
+                diag = torch.sum(
+                    self._crosscov
+                    @ self._sandwiched_linop.U
+                    @ torch.eye(
+                        self._sandwiched_linop.U.shape[1], dtype=torch.float64
+                    ).to(device)
+                    * (
+                        self._crosscov
+                        @ self._sandwiched_linop.V
+                        @ torch.eye(
+                            self._sandwiched_linop.V.shape[1], dtype=torch.float64
+                        ).to(device)
+                    ),
+                    dim=-1,
+                )
         return diag.cpu().numpy()
