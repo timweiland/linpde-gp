2ebdb8b7dabdf1ca1f99bfd35bc12565b1a39f40
Tim Weiland <tim@weiland-lahnstein.de>
2023-12-20 15:09:31 +0100

    `DenseCholeskySolver` as an outer product

--- src/linpde_gp/linops/_dense_cholesky_solver.py
+++ src/linpde_gp/linops/_dense_cholesky_solver.py
@@ -1,12 +1,33 @@
 import numpy as np
 from probnum import linops
-from scipy.linalg import cho_factor, cho_solve
+from scipy.linalg import cho_factor, cho_solve, solve_triangular
 from probnum.linops._vectorize import vectorize_matmat
 import functools
 import torch
 
+from ._outer_product import OuterProductMatrix
 
-class DenseCholeskySolverLinearOperator(linops.LinearOperator):
+
+class InverseTriangularMatrix(linops.LinearOperator):
+    def __init__(self, U: np.ndarray, lower=True):
+        self._U = U
+        self._U_torch = torch.from_numpy(U).to(
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )
+        self._lower = lower
+        super().__init__(U.shape, U.dtype)
+
+    def _matmul(self, x: np.ndarray) -> np.ndarray:
+        return solve_triangular(self._U, x, lower=self._lower)
+
+    def _matmul_torch(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.linalg.solve_triangular(self._U_torch, x, upper=not self._lower)
+
+    def _transpose(self) -> linops.LinearOperator:
+        return InverseTriangularMatrix(self._U.T, lower=not self._lower)
+
+
+class DenseCholeskySolverLinearOperator(OuterProductMatrix):
     def __init__(self, linop: linops.LinearOperator):
         if not linop.is_symmetric or not linop.is_positive_definite:
             raise ValueError(
@@ -14,11 +35,10 @@ class DenseCholeskySolverLinearOperator(linops.LinearOperator):
                 "positive definite linear operators."
             )
         self._linop = linop
-        self._cho = cho_factor(linop.todense())
-        super().__init__(
-            shape=linop.shape,
-            dtype=linop.dtype,
-        )
+        self._cho = cho_factor(linop.todense(), False)
+        self._factor_linop = InverseTriangularMatrix(np.triu(self._cho[0]), lower=False)
+
+        super().__init__(U=self._cho[0])
 
     @functools.cached_property
     def cho_torch(self):
@@ -38,9 +58,6 @@ class DenseCholeskySolverLinearOperator(linops.LinearOperator):
     def _transpose(self) -> linops.LinearOperator:
         return self
 
-    @functools.cached_property
-    def raw_factor(self) -> np.ndarray:
-        if self._cho[1]:
-            # Lower triangular factor
-            return np.tril(self._cho[0])
-        return np.triu(self._cho[0]).T
+    @property
+    def U(self) -> InverseTriangularMatrix:
+        return self._factor_linop
